99b02ea7772faf40c486b118eb29958d3a5c5273
innominata <inno@xray.cat>
2021-11-27 22:26:21 +1300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+namespace StarsandBehemoth
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class StarsandBehemoth : BaseUnityPlugin
+    {
+
+        public new static ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = new ManualLogSource("Behemoth");
+            BepInEx.Logging.Logger.Sources.Add(Logger);
+            Logger.LogInfo("Starsand Behemoth Mod Loading");
+            Harmony.CreateAndPatchAll(typeof(StarsandBehemoth));
+            
+        }
+        [HarmonyPostfix, HarmonyPatch(typeof(ScoloManager), "Start")]
+        public static void Start(ref ScoloManager __instance)
+        {
+            __instance.SpawnScolo(2);
+            
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(EnemyAI), "Start")]
+        public static void EnemyAI(ref EnemyAI __instance)
+        {
+            Logger.LogInfo("%");
+            var scoloTransform = __instance.scoloChild.transform;
+            Logger.LogInfo(__instance.gameObject.activeSelf);
+            Logger.LogInfo(__instance.gameObject.name);
+            Logger.LogInfo("-");
+
+            Logger.LogInfo(scoloTransform.localScale);
+            Logger.LogInfo("-");
+            if (new Random().Next(0, 20) > 17)
+            {
+                scoloTransform.localScale = new Vector3(3, 3, 3);
+                __instance.aiSettings.AI.Health.Set(200f);
+            }
+        }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# Starsand Compass
+ 
+BepInEx mod for Starsand to add simple Cardinal directions to the HUD
+
+## Installation
+
+### Mod Manager (Recommended)
+
+Install either [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) or [r2modman](https://thunderstore.io/package/ebkr/r2modman/) and then install [StarsandCompass from Thunderstore](https://starsand.thunderstore.io/package/innominata/StarsandCompass/)
+
+### Manual Installation
+
+Follow the instructions from the [BepInEx Docs](https://docs.bepinex.dev/articles/user_guide/installation/index.html) to install BepInEx and then extract [the latest release](https://github.com/innominata/StarsandCompass/releases/latest) of the mod to your `BepInEx\plugins\` directory.
+
+## Configuration
+
+None required. Remove the mod or rename the .dll extension to .dllOld to disable
+
+## Support
+
+For support, or to discuss and discover other mods for Starsand, check out the [Starsand Modding Discord](https://discord.gg/ZYVpC6uyY7)
+
+## Changelog
+1.4.0:
+- Compass now works while on camels
+
+1.3.1:
+- Centered compass for oxothuk
+
+1.3.0:
+ - Fixed versioning.
+
+1.2.1: 
+
+ - Fixed an error in console
+ 
+1.2.0:
+
+ - Fixed it on purpose
+
+1.1.0:
+
+ - Broke it by accident
+
+1.0.0:
+
+- Initial release
